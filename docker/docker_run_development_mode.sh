bc462928715c3d6162782da6820c8a0a1d4ac12d
nichelia <nich.elia@gmail.com>
2018-11-18 08:23:41 +0000

    Docker for Mac has some issues with volumes, resolved by adding override file.

--- docker/docker_run_development_mode.sh
+++ docker/docker_run_development_mode.sh
@@ -5,7 +5,11 @@ echo $'\n\t>> Retrieving required Docker images...\n'
 run_dev()
 {
   echo $'\n\t>> Running cctool as local...\n'
-  docker-compose -f "${CCTOOL}/docker/docker-compose.local.yml" -f "${CCTOOL}/docker/docker-compose.local.override.yml" up -d
+  if [ "$(uname)" == "Darwin" ]; then
+    docker-compose -f "${CCTOOL}/docker/docker-compose.local.yml" -f "${CCTOOL}/docker/docker-compose.local.override.yml" up -d
+  else
+    docker-compose -f "${CCTOOL}/docker/docker-compose.local.yml" up -d
+  fi
 }
 
 cctool_set=$(docker images -a | awk '{ print $1,$3 }' | grep cctool_local | awk '{ print $2 }' | wc -l)
