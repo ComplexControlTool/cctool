fac764f816b10e9b34956fbacc02635acc932b6e
nichelia <nich.elia@gmail.com>
2019-04-01 08:33:23 +0100

    Further improvements for docker production release

--- docker/docker_create_custom_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-source "${CCTOOL}/docker/docker_cleanup_custom_images_containers.sh"
-echo $'\n\t>> Creating docker images for cctool...\n'
-docker-compose -f "${CCTOOL}/docker/docker-compose.local.yml" build
-# docker-compose -f docker-compose.prod.yml build
